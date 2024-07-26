a4976b8918c507e551fb87049992c60756c4e0d2
karthickag04 <karthickag04@gmail.com>
2024-07-26 14:19:25 +0530

    updated foodapp database statements

--- mysql/session_05_joins_with_foodapp_db.sql
+++ mysql/session_05_joins_with_foodapp_db.sql
@@ -1,12 +1,24 @@
--- steps to create and work with foodapp 
--- select all by using keyboard shortcuts (ctrl + A) and then execute all at once to create foodapp database
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: 192.168.29.244    Database: foodapp
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+drop database if exists foodapp1;
+create database foodapp1;
+use foodapp1;
 
 
-drop database if exists foodapp;
-create database food;
-use food;
-
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `admins`
